1e3ec63ad2f641ef43b373bed93f8f48e9e4416a
achytent <achytent@gmail.com>
2023-08-20 14:25:10 +0300

    Create header and products list

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # react-sneakers
+
+Tutorial from Archakov Blog with the only difference in CSS. I am using scss without macro-css library.
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,82 @@
-
 function App() {
 
   return (
-    <div className="App">
-       
+    <div className="wrapper">
+       <header>
+        <div className="headerLeft">
+          <img width={40} height={40} src="/img/logo.png" className="mr-15" />
+          <div>
+            <h3>React Sneakers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className="headerRight">
+          <li>
+            <img src="/img/cart.svg" /><span>1205 руб.</span>
+          </li>
+          <li>
+            <img src="/img/user.svg"/>
+          </li>
+        </ul>
+       </header>
+       <div className="content">
+        <h1>Все кроссовки</h1>
+        <div className="sneakers">
+          <div className="card">
+            <img width={133} height={112} src="/img/sneakers/1.jpg" alt="" />
+            <h5>Мужские Кроссовки Nike Air Max 270</h5>
+            <div className="cardBottom">
+              <div className="price">
+                <span>Цена:</span>
+                <b>12999 руб.</b>
+              </div>
+              <button>
+                <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+          <img width={133} height={112} src="/img/sneakers/2.jpg" alt="" />
+          <h5>Мужские Кроссовки Nike Air Max 270</h5>
+          <div className="cardBottom">
+            <div className="price">
+              <span>Цена:</span>
+              <b>12999 руб.</b>
+            </div>
+            <button>
+              <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <img width={133} height={112} src="/img/sneakers/3.jpg" alt="" />
+          <h5>Мужские Кроссовки Nike Air Max 270</h5>
+          <div className="cardBottom">
+            <div className="price">
+              <span>Цена:</span>
+              <b>12999 руб.</b>
+            </div>
+            <button>
+              <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <img width={133} height={112} src="/img/sneakers/4.jpg" alt="" />
+          <h5>Мужские Кроссовки Nike Air Max 270</h5>
+          <div className="cardBottom">
+            <div className="price">
+              <span>Цена:</span>
+              <b>12999 руб.</b>
+            </div>
+            <button>
+              <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
+            </button>
+          </div>
+        </div>
+        </div>
+        
+       </div>
     </div>
   )
 }
