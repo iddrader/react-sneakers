ba633c488c3e3e858638f4c65d35283466b2409f
achytent <achytent@gmail.com>
2023-08-24 12:29:35 +0300

    Display cards with .map and passing props

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,29 @@ import Card from "./components/Card.jsx";
 import Header from "./components/Header.jsx";
 import CartDrawer from "./components/CartDrawer.jsx";
 
+const arr = [
+  {
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 12999,
+    imageURL: "/img/sneakers/1.jpg"
+  },
+  {
+    title: "Кроссовки Puma X Aka Boku Future Rider",
+    price: 8999,
+    imageURL: "/img/sneakers/2.jpg"
+  },
+  {
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 8499,
+    imageURL: "/img/sneakers/3.jpg"
+  },
+  {
+    title: "Кроссовки Puma X Aka Boku Future Rider",
+    price: 8999,
+    imageURL: "/img/sneakers/4.jpg"
+  },
+]
+
 function App() {
   const [showCart, setShowCart] = useState(false);
 
@@ -20,12 +43,14 @@ function App() {
           </div>
         </div>
         <div className="sneakers">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          { arr.map( element => 
+            <Card 
+              title={element.title} 
+              price={element.price} 
+              imageURL={element.imageURL} 
+              key={element.title} 
+            />
+          )}
         </div>
       </div>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,24 @@
-function Card() {
-    return (
-        <div className="card">
-          <div className="favourite">
-            <img src="/img/heart-unliked.svg" alt="" />
-          </div>
-          <img width={133} height={112} src="/img/sneakers/1.jpg" alt="" />
-          <h5>Мужские Кроссовки Nike Air Max 270</h5>
-          <div className="cardBottom">
-            <div className="price">
-              <span>Цена:</span>
-              <b>12999 руб.</b>
-            </div>
-            <img width={32} height={32} src="/img/button-plus.svg" alt="Plus" />
-          </div>
+function Card({title, price, imageURL}) {
+  const onClickButton = () => {
+    alert(title)
+  }
+
+  return (
+      <div className="card">
+        <div className="favourite">
+          <img src="/img/heart-unliked.svg" alt="" />
         </div>
-    )
+        <img width={133} height={112} src={imageURL} alt="" />
+        <h5>{title}</h5>
+        <div className="cardBottom">
+          <div className="price">
+            <span>Цена:</span>
+            <b>{price} руб.</b>
+          </div>
+          <img width={32} height={32} src="/img/button-plus.svg" alt="Plus" onClick={onClickButton}/>
+        </div>
+      </div>
+  )
 }
 
 export default Card;
